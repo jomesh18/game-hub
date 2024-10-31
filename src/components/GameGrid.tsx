0e7e7481d3c80a2540d9967d13e8f7be89fdad3d
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-10-31 17:46:19 +0530

    Display Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,9 +15,9 @@ const GameGrid = () => {
           sm: 1,
           md: 2,
           lg: 3,
-          xl: 5,
+          xl: 4,
         }}
-        spacing={10}
+        spacing={3}
         padding={10}
       >
         {isLoading &&
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,10 +3,11 @@ import useData from "./useData";
 export interface Genre {
   id: number;
   name: string;
+  image_background: string;
 }
 
 const useGenres = () => {
-  return useData<Genre>("/genre");
+  return useData<Genre>("/genres");
 };
 
 export default useGenres;
