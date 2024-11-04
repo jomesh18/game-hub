81412062a551a44e7d4bd302027bec9e4658b7df
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-11-04 15:55:13 +0530

    Clean up the game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -22,7 +22,7 @@ const GameGrid = ({ gameQuery }: Props) => {
           lg: 3,
           xl: 4,
         }}
-        spacing={3}
+        spacing={6}
         padding={10}
       >
         {isLoading &&
